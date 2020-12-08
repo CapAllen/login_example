2e118b509f28bb47e2e24852e2e59971a11526fd
CapAllen <allenjsj@163.com>
2020-12-08 18:28:08 +0800

    update

--- project.py
+++ project.py
@@ -1,24 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Regexp, length
 
+import redis
+
+pool = redis.ConnectionPool(host='localhost')
+redis_con = redis.Redis(connection_pool=pool)
+
 app = Flask(__name__)
 app.secret_key = 'test'
 
-# 定义登录表单
-class LoginForm(FlaskForm):
-    # 域初始化
-    user_phone = StringField('phone', validators=[DataRequired(), length(11,11),
-                            Regexp('^1\d{10}$', 0, '手机号码不合法')],
-                            render_kw={'placeholder': '输入手机号：'})
-    verify_code = StringField('code',validators=[DataRequired('请输入验证码')],
-                            render_kw={'placeholder': '输入手机号：'})
+
+# 获取验证码
+@app.route('/<user_phone>/get_code')
+def get_phone_code(user_phone):
+    user_phone = str(user_phone)
+    if user_phone == '18629577951':
+        verify_code = '1126'
+    else:
+        verify_code = '0701'
+    redis_con.set(user_phone,verify_code)
+    return (verify_code)
+    # print('验证码：',verify_code)
+
+# 检查验证码
+@app.route('/<user_phone>/<code>/check_code')
+def go_check_code(user_phone,code):
+    
+    user_phone = str(user_phone)
+    code = str(code)
+    correct_code = bytes.decode(redis_con.get(user_phone))
+
+    if correct_code == code:
+        return jsonify({'result':True})
+    else:
+        return jsonify({'result':False})
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    form = LoginForm()
-    return render_template('index.html', form=form)
+    return render_template('index.html')
+    
+        
+
 
 @app.route('/login',methods=['GET','POST'])
 def login():
